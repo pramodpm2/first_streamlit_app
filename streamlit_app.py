f2cab1c12afcfe5908f99be1a8dd9b465fa6c89c
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2022-09-22 11:26:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit 
+import pandas
 streamlit.title("hello world")
+
 
 
 streamlit.header(' Breakfast Menu')
@@ -7,3 +9,6 @@ streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔  Hard-Boiled Free-Range Egg')
 streamlit.text("🥑 🍞 Avacodo toast")
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
