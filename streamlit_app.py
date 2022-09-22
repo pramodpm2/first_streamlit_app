4e485a821bedde6a3b416ef28ba89a18ecc53b5d
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2022-09-22 11:23:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.title("hello world")
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')
+streamlit.text("🥑 🍞 Avacodo toast")
