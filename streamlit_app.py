09bb5b9b74ae4a02864c884ba09522093c8959b8
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2022-09-25 15:53:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("insert into fruit_load_list values ('from streamlit');")
 my_data_row = my_cur.fetchall()
 streamlit.header("Fruit load list contains")
 streamlit.dataframe(my_data_row)
